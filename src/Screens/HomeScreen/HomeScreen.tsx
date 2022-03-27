fd2a7582d571ed1d1cb5029eb39b2ae792620cf5
Elias_Silva <inboxsilva1998@gmail.com>
2022-03-27 14:37:28 -0300

    Add axios request and API url to homescreen

--- src/Screens/HomeScreen/HomeScreen.tsx
+++ src/Screens/HomeScreen/HomeScreen.tsx
@@ -1,9 +1,34 @@
 import { StyleSheet, Button, Text, View } from 'react-native';
+import { baseUrl } from '../../constants/baseUrl';
+import React, { useState, useEffect } from 'react';
+
+import axios from 'axios';
 
 export default function HomeScreen({navigation}: {navigation: any}) {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+
+    axios.get(`${baseUrl}`).then((response) => {
+      setPosts(response.data)
+      // console.log(response.data);
+    });
+
+  },[posts]);
+
+  const dados =  posts.map((post: any) => {
+    return (
+        <View key={post.id}>
+          <Text>{post.title}</Text>
+          <Text>{post.body}</Text>
+        </View>
+      )
+  });
+
   return (
     <View style={styles.container}>
       <Text>Essa página é a Home Screen!</Text>
+        {dados}
       <Button
         title="Cadastrar um post"
         onPress={() => navigation.navigate('Register')}
