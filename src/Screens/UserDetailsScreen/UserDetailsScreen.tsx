5a0574250b8e349af29b81aae1e20d8b2fefaef4
Elias_Silva <inboxsilva1998@gmail.com>
2022-03-28 19:08:33 -0300

    Add axios request to userDetailsScreen

--- src/Screens/UserDetailsScreen/UserDetailsScreen.tsx
+++ src/Screens/UserDetailsScreen/UserDetailsScreen.tsx
@@ -1,7 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+
+import axios from 'axios';
 
 export default function UserDetailsScreen() {
+  const [user, setUser] = useState([]);
+
+  useEffect(() => {
+
+    axios.get(`https://jsonplaceholder.typicode.com/users/2`).then((response) => {
+      setUser(response.data)
+      // console.log(response.data)
+    });
+
+  },[user]);
+
   return (
     <View style={styles.container}>
       <Text>Essa página é a de informações de quem postou!</Text>
