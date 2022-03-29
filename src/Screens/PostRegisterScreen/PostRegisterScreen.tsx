5f79c963a914262e8b69460f2a8ddc97c5aa07cd
Elias_Silva <inboxsilva1998@gmail.com>
2022-03-29 00:47:30 -0300

    Add axios request and set data variables to PostRegisterScreen

--- src/Screens/PostRegisterScreen/PostRegisterScreen.tsx
+++ src/Screens/PostRegisterScreen/PostRegisterScreen.tsx
@@ -1,9 +1,31 @@
 import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import axios from 'axios';
+import { baseUrl } from '../../constants/baseUrl';
 
 export default function PostRegisterScreen() {
 
   const [form, setForm] = useState({title: "", body: "", userId: 67});
+
+  const body  = {
+    title: form.title,
+    body: form.body,
+    userId: form.userId
+  }
+
+  const registerPost =()=>{
+    axios.post(`${baseUrl}`, { 
+      title:body.title, body:body.body, userId:body.userId,
+      })
+      .then((res) => {
+          alert("Postado!")
+          console.log(res)
+
+    })
+    .catch((err) => {
+      alert("erro ao postar, tente novamente")
+    })
+  }
 
   return (
     <View style={styles.container}>
